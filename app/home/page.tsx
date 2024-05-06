a9ebde59801ca02eb7ae359978c9a8cf05407e1f
Muhammad Kashif <gmdcf797@gmail.com>
2024-05-06 12:53:46 +0500

    Prismic cms

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,19 +1,18 @@
-import { createClient } from "../prismicio";
+import { createClient } from "../../prismicio";
 import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText } from "@prismicio/react";
 import Link from "next/link";
 export default async function Home() {
   const client = createClient();
-  const page = await client.getSingle("home");
+  const page = await client.getSingle("about");
   return (
     <>
       <Link href={"/home"} className="mx-2 py-2">
         about
       </Link>
       <div className="py-6">
-        <h1>{page.data.site_tile}</h1>
-        <h1>{page.data.description}</h1>
-
-        <PrismicNextImage field={page.data.og_image} className="" />
+        <PrismicRichText field={page.data.title} />
+        <PrismicNextImage field={page.data.image} className="" />
       </div>
     </>
   );
